9e5e43d9a734bbddb440f259f9bae41cb3cfe83d
Al Samir <shahinur1591@gmail.com>
2024-06-30 12:37:03 +0600

    some css fixed

--- src/components/ContentBody.tsx
+++ src/components/ContentBody.tsx
@@ -15,14 +15,14 @@ export default function ContentBody({
     <Bounded as="article">
       <div className="rounded-2xl border-2 border-slate-800 bg-slate-900 px-4 py-10 md:px-8 md:py-20">
         <Heading as="h1">{page.data.title}</Heading>
-        <div className="flex gap-4 text-yellow-400 mt-8">
+        <div className="mt-8 flex flex-wrap gap-4 text-yellow-400">
           {page.tags.map((tag, index) => (
             <span key={index} className="text-xl font-bold">
               {tag}
             </span>
           ))}
         </div>
-        <p className="mt-8 pb-4 border-b border-slate-600 text-xl font-medium text-slate-300">
+        <p className="mt-8 border-b border-slate-600 pb-4 text-xl font-medium text-slate-300">
           {page.data.date}
         </p>
         <div className="prose prose-xl prose-invert mt-4 w-full max-w-none md:mt-10">
